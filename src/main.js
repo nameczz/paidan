3f0ac6db678c650a4e3b07e66704df1c9964edd1
nameczz <nameczz@nameczzdeMacBook-Pro.local>
2019-03-23 16:38:20 +0800

    init project

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
+import './cube-ui'
 import App from './App.vue'
-import router from './router'
-import store from './store'
-
+import router from './router/'
+import store from './store/'
+import 'common/stylus/index.styl'
+import fastclick from 'fastclick'
 Vue.config.productionTip = false
+
+fastclick.attach(document.body)
 
 new Vue({
   router,
---
+++ vue.config.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const webpack = require('webpack')
+
+function resolve (dir) {
+  return path.join(__dirname, dir)
+}
+module.exports = {
+  css: {
+    loaderOptions: {
+      stylus: {
+        'resolve url': true,
+        import: ['./src/theme']
+      }
+    }
+  },
+  pluginOptions: {
+    'cube-ui': {
+      postCompile: true,
+      theme: true
+    }
+  },
+  chainWebpack (config) {
+    config.resolve.alias
+      .set('components', resolve('src/components'))
+      .set('common', resolve('src/common'))
+      .set('api', resolve('src/api'))
+
+    config
+      .plugin('context')
+      .use(webpack.ContextReplacementPlugin, [/moment[/\\]locale$/, /zh-cn/])
+  }
+}
